0cd98412ced8ca213a8dc2bee410efa37eea129e
TheBoss3696 <theboss3696@gmail.com>
2022-06-10 10:17:31 +1200

    Update Main File.py
    Added Combobox

--- Main File.py
+++ Main File.py
@@ -1,6 +1,7 @@
 
 from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk
 
 
 # quit subroutine
@@ -9,7 +10,7 @@ def quit():
 
 
 
-def print_camp_details():
+def print_store_details():
     global item_count, total_entries
     item_count = 0
     Label(mw, font='bold', text="Row").grid(column=0, row=7)
@@ -33,16 +34,12 @@ def append_name():
 
     # Lists all possible boxes able to be filled and adds it to the rows
     entries = [entry_cname, entry_rnumber, entry_ihired, entry_numhired]
-    if len(entry_cname.get()) != 0 and len(entry_rnumber.get()) != 0 and len(entry_ihired.get()) != 0 and \
-            len(entry_numhired.get()) != 0:
-
-
-        store_details.append([entry_cname.get(), entry_rnumber.get(), entry_ihired.get(), entry_numhired.get()])
-        entry_cname.delete(0, 'end')
-        entry_rnumber.delete(0, 'end')
-        entry_ihired.delete(0, 'end')
-        entry_numhired.delete(0, 'end')
-        total_entries += 1
+    store_details.append([entry_cname.get(), entry_rnumber.get(), entry_ihired.get(), entry_numhired.get()])
+    entry_cname.delete(0, 'end')
+    entry_rnumber.delete(0, 'end')
+    entry_ihired.delete(0, 'end')
+    entry_numhired.delete(0, 'end')
+    total_entries += 1
 
 
 
@@ -58,15 +55,15 @@ def delete_row():
     Label(mw, text="       ").grid(column=2, row=item_count + 7)
     Label(mw, text="       ").grid(column=3, row=item_count + 7)
     Label(mw, text="       ").grid(column=4, row=item_count + 7)
-    print_camp_details()
+    print_store_details()
 
 
 # create the buttons and labels
 def setup_buttons():
     global store_details, entry_cname, entry_rnumber, entry_ihired, entry_numhired, total_entries, delete_item
-    Button(mw, text="Quit", command=quit).grid(column=1, row=0)
+    Button(mw, text="Quit", command=quit).grid(column=3, row=1)
     Button(mw, text="Append Details", command=append_name).grid(column=0, row=1)
-    Button(mw, text="Print Details", command=print_camp_details).grid(column=1, row=1)
+    Button(mw, text="Print Details", command=print_store_details).grid(column=1, row=1)
     Label(mw, text="Name").grid(column=0, row=2)
     entry_cname = Entry(mw)
     entry_cname.grid(column=1, row=2)
@@ -74,7 +71,8 @@ def setup_buttons():
     entry_rnumber = Entry(mw)
     entry_rnumber.grid(column=1, row=3)
     Label(mw, text="Item Hired").grid(column=0, row=4)
-    entry_ihired = Entry(mw)
+    item = StringVar()
+    entry_ihired = ttk.Combobox(mw, textvariable = item, state = "readonly", values =("BBQ LED_lights Balloons Party_animals"),width = 17)
     entry_ihired.grid(column=1, row=4)
     Label(mw, text="NO. Hired").grid(column=0, row=5)
     entry_numhired = Entry(mw)
@@ -88,7 +86,7 @@ def setup_buttons():
 # start the program running
 def main():
     global mw
-    global store_details, entry_name, entry_age, entry_gender, total_entries
+    global store_details, total_entries
     store_details = []
     total_entries = 0
     mw = Tk()
